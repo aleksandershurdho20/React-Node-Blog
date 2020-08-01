d13d4cd9361352f39d45b4625b6754a4b42224b2
aleksander <aleksandershurdho@gmail.com>
2020-08-01 22:48:53 +0200

    working in authentication

--- server/server.js
+++ server/server.js
@@ -4,10 +4,22 @@ const cors = require('cors')
 require('dotenv').config();
 const app = express ();
 app.enable('trust proxy')
-
+const mongoose = require('mongoose')
 app.use(cors ({origin:'*'}))
 //* =>  all origins to accept form
 app.use(bodyParser.json())
+const authRoutes = require('./routes/authRoutes')
+//Creating API
+
+app.use("api/v1/auth", authRoutes)
+
+//Connect to Database
+const DB = process.env.DATABASE
+mongoose.connect(DB,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true
+}).then(()=>console.log('connected to the database'))
 const port = process.env.port || 5000;
 app.listen(port, () =>{
     console.log(`listening at port ${port}`)
