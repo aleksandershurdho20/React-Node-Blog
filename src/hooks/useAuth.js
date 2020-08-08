83b4fc4facecfd676855c80a0442b2c43cd4ec00
aleksander <aleksandershurdho@gmail.com>
2020-08-08 22:20:49 +0200

    fixed hook issue

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -5,9 +5,25 @@ import { useHistory } from "react-router-dom";
 export const useAuth = () => {
     let history = useHistory();
 
-  
+    const [formSignUp, setformSignUp]=useState({
+    email:'',
+    password:''
+    })
 
 
+    const signupUser = async (email,password) => {
+        try{
+            const config = {headers :{"Content-Type":"application/json"}}
+            const body = {email,password};
+            const res  = await api.post('/api/signup',body,config);
+            console.log('Data from Api ',res)
+
+        }
+        catch(err){
+            console.log(err)
+
+        }
+    }
 
     const loginUser = async (email, password) => {
         try{
@@ -26,5 +42,5 @@ export const useAuth = () => {
         }
       }
 
-    return [loginUser];
+    return [formSignUp, setformSignUp, loginUser,  signupUser];
 }
