324c7fe0007486bd764abca672b4dfae7a29d8a7
aleksander <aleksandershurdho@gmail.com>
2020-08-08 22:07:04 +0200

    fixed routing issue

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -140,7 +140,7 @@ export default function Login() {
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="/SignUp" variant="body2">
+                <Link to="/SignUp" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -150,7 +150,7 @@ export default function SignUp() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="/Login" variant="body2">
+              <Link href="/" variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
